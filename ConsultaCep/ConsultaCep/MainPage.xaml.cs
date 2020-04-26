636bdc16519779a60c6fa9650d7a28e45b90793d
Fabio Galante Mans <fabiogalantemans@gmail.com>
2020-04-26 16:29:14 -0300

    Xamarin - App Cep

--- ConsultaCep/ConsultaCep/MainPage.xaml.cs
+++ ConsultaCep/ConsultaCep/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using ConsultaCep.Servico;
+using ConsultaCep.Servico.Modelo;
 using Xamarin.Forms;
 
 namespace ConsultaCep
@@ -11,6 +14,54 @@ namespace ConsultaCep
         public MainPage()
         {
             InitializeComponent();
+
+            BotaoBuscarCep.Clicked += OnBuscarCep;
+        }
+
+        private void OnBuscarCep(object sender, EventArgs e)
+        {
+            string cep = Cep.Text;
+
+            if (OnIsValidCep(cep))
+            {
+                try
+                {
+                    var end = ViaCepServico.BuscarEnderecoViaCEP(cep.Trim());
+
+                    if (end != null)
+                    {
+                        Result.Text = $"Endereço: {end.Logradouro} de {end.bairro} {end.localidade},{end.uf} ";
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO", $"O endereço não foi encontrado para o CEP informado: {cep}", "OK");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("ERRO CRÍTICO", ex.Message, "OK");
+                }
+            }
+        }
+
+        private bool OnIsValidCep(string cep)
+        {
+            if (string.IsNullOrEmpty(cep) || cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "CEP inválido! O CEP deve conter 8 caracteres.", "OK");
+                return false ;
+            }
+
+
+            if (!int.TryParse(cep, out _))
+            {
+                DisplayAlert("ERRO", "CEP inválido! O CEP deve ser composto apenas por números.", "OK");
+                return false;
+            }
+
+
+            return true;
         }
     }
 }
---
+++ ConsultaCep/ConsultaCep/Servico/ViaCepServico.cs
@@ -0,0 +1,21 @@
+﻿using ConsultaCep.Servico.Modelo;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ConsultaCep.Servico
+{
+    public class ViaCepServico
+    {
+        private static string _enderecoUrl = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string novoEnderecoUrl = string.Format(_enderecoUrl, cep);
+            var wc = new WebClient();
+            string conteudo = wc.DownloadString(novoEnderecoUrl);
+            var end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+            return end.cep == null ? null : end;
+        }
+
+    }
+}
